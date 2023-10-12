296b13dd94f4381fce101305fc3fecfd3dddc389
sujin <soojin9877@naver.com>
2023-10-12 22:09:41 +0900

    design: 메뉴 버튼 간격 조정

--- client/src/components/menu/menu.tsx
+++ client/src/components/menu/menu.tsx
@@ -2,6 +2,8 @@
 
 import { useAtom, useSetAtom } from "jotai";
 import { modalAtom, isTimerVisibleAtom, timerAtom } from "@/jotai/atom";
+import Image from "next/image";
+import active_user_icon from "/public/active_user.svg";
 import TimerSetting from "../timer/timerSetting";
 import Timer from "../timer/timer";
 import style from "./menu.module.scss";
@@ -38,7 +40,7 @@ const Menu = (props: IMenu) => {
                     <button className={style.screen_share} aria-label="화면 공유하기"></button>
                 </li>
                 <li className={style.active_user_number}>
-                    <img src="/active_user.svg" alt="접속자 수" />
+                    <Image src={active_user_icon} alt="접속자 수" />
                     <span className={style.active_circle}>●</span>
                     <span>2</span>
                 </li>
