5277504aefa8308384b64b4f4e10419cc7ea2b21
leehyeonseop <gustjq4146@naver.com>
2023-11-14 22:15:24 +0900

    feat: 사용자 정보 컴포넌트화

---
+++ client/src/components/userInfo/userMenu/userMenu.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import UserMenuItem from "../userMenuItem/userMenuItem";
+import style from "./userMenu.module.scss";
+
+interface IUserMenu {
+    className?: string;
+}
+
+const USER_MENU = [
+    {
+        icon: <Image width={24} height={24} src="/setting.svg" alt="환경설정 아이콘" />,
+        label: "something Setting",
+    },
+    {
+        icon: <Image width={24} height={24} src="/door-exit.svg" alt="로그아웃 아이콘" />,
+        label: "Logout",
+    },
+];
+
+const UserMenu = (props: IUserMenu) => {
+    const { className } = props;
+
+    return (
+        <div className={`${style.wrapper} ${className}`}>
+            {USER_MENU.map((menu, index) => (
+                <UserMenuItem key={index} icon={menu.icon} label={menu.label} />
+            ))}
+        </div>
+    );
+};
+
+export default UserMenu;
