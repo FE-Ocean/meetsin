0b43e0b53374f2ea6e2b926a1b7412abd3b80862
leehyeonseop <gustjq4146@naver.com>
2023-11-14 22:15:24 +0900

    feat: 사용자 정보 컴포넌트화

--- client/src/app/test/page.tsx
+++ client/src/app/test/page.tsx
@@ -1,9 +1,15 @@
 import ProfileImage from "@/components/common/profile_image/ProfileImage";
+import UserInfo from "@/components/userInfo/userInfo";
 import { imageSizeType } from "@/constants/ImageSize.type";
 import React from "react";
 
 function page() {
-    return <ProfileImage src="https://picsum.photos/id/237/200/300" size={imageSizeType.large} />;
+    return (
+        <>
+            <ProfileImage src="https://picsum.photos/id/237/200/300" size={imageSizeType.large} />
+            <UserInfo direction="bottom" />
+        </>
+    );
 }
 
 export default page;
