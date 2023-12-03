90ae6f3d89796bac02e4e412cc11d8529b8e6106
leehyeonseop <gustjq4146@naver.com>
2023-12-03 13:30:18 +0900

    feat: 방 링크 복사 버튼 구현

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 
+import LinkCopyButton from "@/components/menu/linkCopyButton/linkCopyButton";
+
 const Home = () => {
-    return <button>임시 로그인 버튼 추가 예정</button>;
+    return (
+        <>
+            <button>임시 로그인 버튼 추가 예정</button>
+            <LinkCopyButton />
+        </>
+    );
 };
 export default Home;
---
+++ client/src/components/menu/linkCopyButton/linkCopyButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState } from "react";
+import style from "./linkCopyButton.module.scss";
+
+interface IProps {
+    disabledTime?: number;
+}
+
+const LinkCopyButton = (props: IProps) => {
+    const { disabledTime = 2000 } = props;
+
+    const [isCopied, setIsCopied] = useState(false);
+
+    const handleLinkCopy = async () => {
+        try {
+            const url = window.location.href;
+            await navigator.clipboard.writeText(url);
+            setIsCopied(true);
+            setTimeout(() => setIsCopied(false), disabledTime);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const buttonStyle = isCopied ? style.link_after_copy : style.link_before_copy;
+
+    return <button className={buttonStyle} onClick={handleLinkCopy} disabled={isCopied} />;
+};
+
+export default LinkCopyButton;
