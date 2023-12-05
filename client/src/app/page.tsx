0243e9fff33627d59e232123f64944fd9e8286e6
sujin <soojin9877@naver.com>
2023-12-05 17:25:43 +0900

    feat: 서비스 워커 등록

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,19 @@
 "use client";
+import { useEffect } from "react";
 
 const Home = () => {
+    useEffect(() => {
+        const registerServiceWorker = async () => {
+            if (!("serviceWorker" in navigator)) {
+                alert("이 브라우저는 서비스 워커 제공 X");
+                return;
+            }
+
+            await navigator.serviceWorker.register("/serviceWorker.js");
+        };
+        registerServiceWorker();
+    }, []);
+
     return <button>임시 로그인 버튼 추가 예정</button>;
 };
 export default Home;
