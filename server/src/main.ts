f77f7b66bda85bb7929d3063eb564a7e0adc8c54
sujin <soojin9877@naver.com>
2024-04-01 22:38:41 +0900

    feat: class-transformer, class-validator 설치 및 적용

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,27 +1,30 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import session from "express-session";
-import passport from "passport"
+import passport from "passport";
 import dotenv from "dotenv";
+import { ValidationPipe } from "@nestjs/common";
 
 dotenv.config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.use(session({
-        secret: process.env.SESSION_SECRET, // temp
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            httpOnly: true,
-            secure: false,
-            maxAge: 30000,
-        },
-    }))
+    app.use(
+        session({
+            secret: process.env.SESSION_SECRET, // temp
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                httpOnly: true,
+                secure: false,
+                maxAge: 30000,
+            },
+        }),
+    );
 
-    app.use(passport.initialize())
-    app.use(passport.session())
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     // CORS 설정
     app.enableCors({
@@ -29,6 +32,9 @@ async function bootstrap() {
         methods: ["GET", "POST"],
         credentials: true,
     });
+
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
     await app.listen(8000);
 }
 bootstrap();
--- server/src/rooms/dto/create-room.dto.ts
+++ server/src/rooms/dto/create-room.dto.ts
@@ -1,3 +1,10 @@
+import { IsString, Length, Matches } from "class-validator";
 export class CreateRoomDto {
+    @Length(1, 10, {
+        message: "roomName의 글자수는 1-10자 입니다.",
+    })
+    @Matches(/^[\w\.\-가-힣ㄱ-ㅎㅏ-ㅣ]$/, {
+        message: "한글, 영문, 숫자, ., -, _만 가능합니다.",
+    })
     readonly roomName: string;
 }
