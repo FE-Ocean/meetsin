8e2a293f152b2a0d2cffe16f0654962c4c2bc33f
sujin <soojin9877@naver.com>
2024-04-30 21:06:08 +0900

    modify: roomName 최대 10자에서 20자로 변경

--- server/src/rooms/dto/create-room.dto.ts
+++ server/src/rooms/dto/create-room.dto.ts
@@ -1,9 +1,9 @@
 import { Length, Matches } from "class-validator";
 export class CreateRoomDto {
-    @Length(1, 10, {
-        message: "roomName의 글자수는 1-10자 입니다.",
+    @Length(1, 20, {
+        message: "roomName의 글자수는 1-20자 입니다.",
     })
-    @Matches(/^[\w\.\-가-힣ㄱ-ㅎㅏ-ㅣ]{1,10}$/, {
+    @Matches(/^[\w\.\-가-힣ㄱ-ㅎㅏ-ㅣ]{1,20}$/, {
         message: "한글, 영문, 숫자, ., -, _만 가능합니다.",
     })
     readonly roomName: string;
