1b2c798b28d6e3413723258864ca2fef79d26b33
sujin <soojin9877@naver.com>
2024-06-24 21:11:01 +0900

    fix: 소켓 파일 충돌 해결

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -2,6 +2,4 @@ import { io } from "socket.io-client";
 
 const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:8000";
 
-const socketBaseOptions = { autoConnect: true };
-
-export const chatSocket = io(`${URL}/chat`, socketBaseOptions);
+export const chatSocket = io(`${URL}/chat`, { autoConnect: true });
