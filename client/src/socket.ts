b17812cea2feeb6ea6476ab815523cfb7abba63d
donkeeman <doonkeeemaan@gmail.com>
2024-07-17 23:33:20 +0900

    fix: 웹소켓 옵션 수정

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -2,4 +2,4 @@ import { io } from "socket.io-client";
 
 const URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
-export const chatSocket = io(`${URL}/chat`, { autoConnect: true });
+export const chatSocket = io(`${URL}/chat`, { autoConnect: true, withCredentials: true, transports: ["websocket", "polling", "flashsocket"]  });
