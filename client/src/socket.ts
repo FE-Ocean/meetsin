dd01dac44a87c3e50e727f46c91b2c163ca1ee2c
donkeeman <doonkeeemaan@gmail.com>
2024-07-24 23:00:06 +0900

    Update client/src/socket.ts
    Co-authored-by: Sujin <102905624+etoile-j@users.noreply.github.com>

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -2,4 +2,4 @@ import { io } from "socket.io-client";
 
 const URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
-export const roomSocket = io(`${URL}/room`, { autoConnect: true, withCredentials: true, transports: ["websocket", "polling", "flashsocket"]  });
+export const roomSocket = io(`${URL}/room`, { autoConnect: true, withCredentials: true, transports: ["websocket", "polling"]  });
