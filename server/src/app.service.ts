8ccd2d36d0ff0b074b4c235c9c7c58e40d54d2d8
sujin <soojin9877@naver.com>
2024-07-28 14:10:03 +0900

    호스트키 재생성

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!!cache hit 제거";
+        return "Hello meetsin!!호스트키 재생성";
     }
 }
