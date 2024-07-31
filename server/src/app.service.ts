0bc8660479bc82984adc9c91d13f773e46624475
sujin <soojin9877@naver.com>
2024-07-31 21:17:49 +0900

    known_hosts 제거

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! known_hosts 넣";
+        return "Hello meetsin!! known_hosts 제거";
     }
 }
