f13a33defd85411138edfce459653059f2b523d9
sujin <soojin9877@naver.com>
2024-08-06 14:19:05 +0900

    재가동

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! ssh-keyscan 추가";
+        return "Hello meetsin!! ssh-keyscan 추가 재가동";
     }
 }
