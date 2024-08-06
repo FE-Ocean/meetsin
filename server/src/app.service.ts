744bcf9d78baf488da5c3b059496cb8684a6494c
sujin <soojin9877@naver.com>
2024-08-06 16:34:24 +0900

    StrictHostKeyChecking no로 3

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! StrictHostKeyChecking no로2";
+        return "Hello meetsin!! StrictHostKeyChecking no로 3";
     }
 }
