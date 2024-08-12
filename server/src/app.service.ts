4a37288b3fc5616747dcd7baafa6fd382fc2e868
sujin <soojin9877@naver.com>
2024-08-12 23:21:23 +0900

    구 스크립트 실행

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! cd server_meetsin을 최상단으로";
+        return "Hello meetsin!! 구 스크립트";
     }
 }
