96c775a8279125b81f1401b96d7009458e07de6d
sujin <soojin9877@naver.com>
2024-08-12 15:24:11 +0900

    master -> 1.0.3

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! 구 스크립트로";
+        return "Hello meetsin!! master -> 1.0.3";
     }
 }
