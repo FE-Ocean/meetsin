951ed5fc900c5798dcbb75e9bb7fa3502a3970f3
sujin <soojin9877@naver.com>
2024-08-12 23:42:41 +0900

    스크립트를 cat으로

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! 구 스크립트에서 SSH Commands 위 스크립트 추가 후 수정";
+        return "Hello meetsin!! 구 스크립트에서 SSH Commands 위 스크립트 추가 후 스크립트 수정";
     }
 }
