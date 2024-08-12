e87542efac3457fc82fb72be6f5082c77a8c9fea
sujin <soojin9877@naver.com>
2024-08-12 15:26:09 +0900

    v1.0.3으로 변경

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! master -> 1.0.3";
+        return "Hello meetsin!! master -> v1.0.3";
     }
 }
