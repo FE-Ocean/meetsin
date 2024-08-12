bacbe25b3ec54dc1bf248b4a35c454222ad9e061
sujin <soojin9877@naver.com>
2024-08-12 15:33:31 +0900

    cd server_meetsin을 최상단으로

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! master -> v1.0.3";
+        return "Hello meetsin!! cd server_meetsin을 최상단으로";
     }
 }
