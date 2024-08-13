16b9ea5d3a16718ff322a91113feace9311e7bea
sujin <soojin9877@naver.com>
2024-08-13 22:11:32 +0900

    5로 변경

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! 이게 보이면3";
+        return "Hello meetsin!! 이게 보이면 5";
     }
 }
