8a0515d815dc12c66f8e03b3afbb071bcaec6edf
sujin <soojin9877@naver.com>
2024-08-13 21:54:25 +0900

    strip_components 추가

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! github.workspace";
+        return "Hello meetsin!! 이게 보이면";
     }
 }
