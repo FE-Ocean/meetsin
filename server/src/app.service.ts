5e605f24c1e5493bf0c66525b967f380b83f6786
sujin <soojin9877@naver.com>
2024-08-13 16:17:57 +0900

    ls

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! scp 명령어 추가";
+        return "Hello meetsin!! ls 확인";
     }
 }
