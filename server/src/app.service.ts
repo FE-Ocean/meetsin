a606acbfe83a5f51e40cb2fe95d7a7e7d165ff94
sujin <soojin9877@naver.com>
2024-08-13 17:31:26 +0900

    scp source 수정

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! package.json";
+        return "Hello meetsin!! scp source 수정";
     }
 }
