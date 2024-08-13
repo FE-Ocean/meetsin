560b3be50aad0c591e6ed741e423bc36d5755da9
sujin <soojin9877@naver.com>
2024-08-13 17:26:30 +0900

    package.json 가

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! pm2 명령어 수정";
+        return "Hello meetsin!! package.json";
     }
 }
