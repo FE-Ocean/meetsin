fa5770ad2c654b95b1a15378b248a19d942e8fa1
sujin <soojin9877@naver.com>
2024-08-13 21:23:00 +0900

    github.workspace

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! ggg..whwhwh";
+        return "Hello meetsin!! github.workspace";
     }
 }
