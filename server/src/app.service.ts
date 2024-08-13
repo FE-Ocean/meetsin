661c8f05376332b9ea7bb21f690945c1d2658fa2
sujin <soojin9877@naver.com>
2024-08-13 16:45:33 +0900

    pm2 명령어 수정

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! 경로 변경";
+        return "Hello meetsin!! pm2 명령어 수정";
     }
 }
