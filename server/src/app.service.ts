b078854d957ac67b8e60fbfa23c23efd3976fb09
sujin <soojin9877@naver.com>
2024-08-13 17:40:34 +0900

    yarn.lock 하나만

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! scp source 수정";
+        return "Hello meetsin!! yarn.lock만";
     }
 }
