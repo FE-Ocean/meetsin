078edb398e86f7a684918ceed20665c677eebfe2
sujin <soojin9877@naver.com>
2024-08-28 21:36:24 +0900

    디디디디

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! 인스톨부터";
+        return "Hello meetsin!! dist는 과연";
     }
 }
