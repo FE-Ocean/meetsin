e4bb234b67088c15343c6260e32f6ecd309ff41d
sujin <soojin9877@naver.com>
2024-08-28 16:02:14 +0900

    yarn lock test

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! 과연 올바른";
+        return "Hello meetsin!! yarn lock test";
     }
 }
