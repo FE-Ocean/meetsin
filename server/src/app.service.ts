92c21fe4801ddc4a3ad5ca0f55e96439dc7179a7
sujin <soojin9877@naver.com>
2024-08-28 15:18:42 +0900

    같은 걸로 다시

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello meetsin!! 이게 보이면 5! 되어볼래";
+        return "Hello meetsin!! 이게 보이면 5!!";
     }
 }
