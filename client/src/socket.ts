e6396b63092414519c05e707c10f49f1b742d35c
Sujin <102905624+etoile-j@users.noreply.github.com>
2024-09-24 15:32:23 +0900

    fix: room 소켓 연결/해제 시 DB에 저장/삭제 정상화 (#46)
    * fix: roomSocket 자동 연결 -> 수동으로 변경
    
    * fix: removeUserFromRoom 함수 정상 작동

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -2,4 +2,8 @@ import { io } from "socket.io-client";
 
 const URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
-export const roomSocket = io(`${URL}/room`, { autoConnect: true, withCredentials: true, transports: ["websocket", "polling"]  });
+export const roomSocket = io(`${URL}/room`, {
+    autoConnect: false,
+    withCredentials: true,
+    transports: ["websocket", "polling"],
+});
