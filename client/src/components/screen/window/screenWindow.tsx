44c340f4541ca15ed13d020f7eb1aad6dd4d57a2
sujin <soojin9877@naver.com>
2024-12-03 15:37:51 +0900

    refactor: 공유된 화면 레이아웃 변경 및 개선

--- client/src/components/screen/window/screenWindow.tsx
+++ client/src/components/screen/window/screenWindow.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 import { screenShareStateAtom } from "@/jotai/atom";
 import Screen from "@/components/screen/screen";
-import style from "./screenWindow.module.scss";
 import { IPeer, IScreenShareState } from "@/types/peer.type";
 import { useAtomValue } from "jotai";
+import style from "./screenWindow.module.scss";
 
 interface IProps {
-    peerList: Map<string, IPeer>
-    className: string
+    peerList: Map<string, IPeer>;
+    className: string;
 }
 
 const ScreenWindow = ({ peerList, className }: IProps) => {
@@ -16,15 +16,25 @@ const ScreenWindow = ({ peerList, className }: IProps) => {
     });
 
     const screenShareState = useAtomValue(screenShareStateAtom);
-    const gridCols = Array.from(peerList.values()).filter(peer => peer.stream).length > 4 ? "over" : "under";
+    const streamCount = Array.from(peerList.values()).filter((peer) => peer.stream).length;
+    // const gridCols = Array.from(peerList.values()).filter(peer => peer.stream).length > 4 ? "over" : "under";
     // TODO: 누군가 화면 공유 시작/중지할 때마다 리스트를 새로 그려서 깜빡이는 거 같은데 방법이 없을지?
     return (
-        <div className={`${style.screen_window} ${style[gridCols]} ${className}`}>
-            {screenShareState !== IScreenShareState.NOT_SHARING && Array.from(peerList.values()).map((peer, index) => {
-                if(peer.stream) {
-                    return <Screen currentStream={peer.stream} key={index} userName={peer.user.userName} />;
-                }
-            })}
+        <div className={style.screen_window}>
+            <div className={`${style.screens} ${streamCount >= 3 ? style.wrap : ""}`}>
+                {screenShareState !== IScreenShareState.NOT_SHARING &&
+                    Array.from(peerList.values()).map((peer, index) => {
+                        if (peer.stream) {
+                            return (
+                                <Screen
+                                    currentStream={peer.stream}
+                                    key={index}
+                                    userName={peer.user.userName}
+                                />
+                            );
+                        }
+                    })}
+            </div>
         </div>
     );
 };
