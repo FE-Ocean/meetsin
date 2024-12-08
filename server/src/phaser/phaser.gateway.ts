23cefbea527c9deda7635eaf53631227ddf7d1fa
sujin <soojin9877@naver.com>
2024-12-08 14:55:19 +0900

    change: 캐릭터 처음 생성 위치 맵 중앙으로 변경

--- server/src/phaser/phaser.gateway.ts
+++ server/src/phaser/phaser.gateway.ts
@@ -61,8 +61,8 @@ export class PhaserGateway implements OnGatewayConnection, OnGatewayInit, OnGate
 
         const playerInfo = {
             rotation: 0,
-            x: 0,
-            y: 0,
+            x: 360,
+            y: 192,
             playerId: socket.id,
             user: socket.data.user,
         };
